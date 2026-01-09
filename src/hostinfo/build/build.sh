f8f2e7cad169184366bec9ba294132256b3c19ff
gaoxy.franklin <gaoxy.franklin@byte.com>
2026-01-09 17:18:07 +0800

    refactor: Split build and release jobs like containerd

--- src/hostinfo/build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ../src
-go build -o hostinfo main.go
-
-
-docker build -t registry.cn-hangzhou.aliyuncs.com/kubernetes_install/hostinfo:v0.0.1 .
-docker push registry.cn-hangzhou.aliyuncs.com/kubernetes_install/hostinfo:v0.0.1
--- src/hostinfo/tools/scripts/commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-HOSTNAME=`hostname`
-
-git add .
-git commit -m "auth commit from ${HOSTNAME}"
-git push github main
-git push gitee main
